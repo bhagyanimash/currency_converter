61fc07d3e2573cc710eb717e05e693ba71ade882
bhagyanimash <bhagyanimash2000@gmail.com>
2024-07-19 00:22:24 +0530

    add: MainPage, setup tailwind CSS

---
+++ client/src/pages/MainPage.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+export default function MainPage() {
+  return (
+    <div>
+      <h1>Convert Your Currencies</h1>
+      <p>
+        Welcome to "Convert Your Currencies Today"! This application allows you
+        to easily convert currencies based on the latest exchange rates. Whether
+        you're planning a trip, managing your finances, or simply curious about
+        the value of your money in different currencies, this tool is here to
+        help.
+      </p>
+    </div>
+  );
+}
